0b26b95137a50ee70316dd68869d9ebc1eb0ec5b
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-10-17 20:08:08 -0400

    initial download and test console

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -8,3 +8,6 @@
   - We never tamper with state: `healthPoints++`, `healthPoints--` or `someState.push(item)` is FORBIDDEN
   - We use the dedicated "state updater" to schedule a state change: `setHealthPoints(healthPoints + 1)`
 */
+
+
+console.log("Hiya cutie!");
